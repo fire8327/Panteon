710307dd23d0ac417e90e061d4642a1785cb6c4c
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-02-17 08:50:11 +0000

    upd galery

--- Assets/JS/slider.js
+++ Assets/JS/slider.js
@@ -1,11 +1,11 @@
 const swiper = new Swiper('.galery-1', {
     // Optional parameters
-    loop: true,
+    rewind: true,
 
     // Navigation arrows
     navigation: {
-      nextEl: '.next',
-      prevEl: '.prev',
+      nextEl: '.next-galery1',
+      prevEl: '.prev-galery1',
     },
   
     // And if we need scrollbar
@@ -14,8 +14,46 @@ const swiper = new Swiper('.galery-1', {
     },
     breakpoints: {
         // when window width is >= 320px
+        320: {
+          slidesPerView: 1.5,
+          spaceBetween: 30
+        },
+        768: {
+          slidesPerView: 2.5,
+          spaceBetween: 30
+        },
         1024: {
           slidesPerView: 4,
+          spaceBetween: 30
+        }
+      }
+  });
+const swiper2 = new Swiper('.galery-2', {
+    // Optional parameters
+    rewind: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.next-galery2',
+      prevEl: '.prev-galery2',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1.3,
+          spaceBetween: 30
+        },
+        768: {
+          slidesPerView: 1.5,
+          spaceBetween: 30
+        },
+        1024: {
+          slidesPerView: 3,
           spaceBetween: 30
         }
       }
